259b329077c9b41335800b4f221585b8c6e94ed5
Marselino Adimas 200710640 <marselinoadimas2001@gmail.com>
2022-09-13 14:23:21 +0700

    gd view binding

---
+++ app/src/main/java/com/example/view_binding_10640/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10640
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1","Makani iwak","Biar iwak e lemu"),
+        Task("Chapter 2","Nguras","Biar resik"),
+        Task("Chapter 3","Ke Kampus","Di suruh bapak"),
+        Task("Chapter 4","Selesai Kelas","Mabok"),
+        Task("Chapter 5","Pulang","ke rumah")
+    )
+}
